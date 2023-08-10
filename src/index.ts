b3a1a9462ae42848c572451e87772f8f58f0d7c9
nayanrabiul <nayan.rabiul@gmail.com>
2023-08-10 11:40:15 +0600

    workflow created

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-console.log("test");
+//take input
 
-let a = 1;
+//search popular sites : daraz,bdshop etch
+
+//scaping the data into a json file
+
+//feed ai to get the comparison tofind one to thress  best price for the product searched
+
+//output the best price and the link to the product
